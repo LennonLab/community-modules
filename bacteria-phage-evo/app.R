d7a966319937fdc8268a75a751b8d6c40467a241
Jay T. Lennon <lennonj@indiana.edu>
2018-07-20 10:38:31 -0400

    deployed eco & evo modules

--- bacteria-phage-evo/app.R
+++ bacteria-phage-evo/app.R
@@ -1,3 +1,11 @@
+# After modifying code and running app locally, you need to set up the account
+# Visit https://www.shinyapps.io and get token then run following in console:
+# rsconnect::setAccountInfo(name='lennonlab', token='F8514597B861B8FD199A346586E3EF58', secret='owstvIzgllZN3hxqTIXzXKOEwB7Mr0ayiPcOus6R')
+# Then run following in console to deploy app
+# rsconnect::deployApp('/Users/lennonj/GitHub/community-modules/bacteria-phage-evo/')
+# You site should be ready to use: https://lennonlab.shinyapps.io/bacteria-phage-evo/
+
+
 # rsconnect::setAccountInfo(name='jaytlennon', token='F8514597B861B8FD199A346586E3EF58', secret='owstvIzgllZN3hxqTIXzXKOEwB7Mr0ayiPcOus6R')
 # rsconnect::deployApp('/Users/lennonj/GitHub/community-modules/bacteria-phage-evo/')
 
